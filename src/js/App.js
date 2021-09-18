a71b26f9b5b73cb4805469baa204f294a79b6b33
Marek Gerszendorf <marek.gerszendorf@wp.pl>
2021-09-18 12:05:18 +0200

    Add popup

--- src/js/App.js
+++ src/js/App.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import '../css/App.css';
 import '../css/Day.css';
 import '../css/Popup.css';
@@ -8,10 +10,29 @@ import Popup from './Popup';
 import NextFiveDays from './NextFiveDays';
 
 function App() {
+  const [button, setButton] = useState(true);
+  const [city, setCity] = useState(0);
+
+  function handleBtn() {
+    setButton(!button);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setButton(false);
+  }
+
   return (
     <div className="App">
       <Day/>
-      <Popup />
+      <Popup 
+        button={button}
+        setButton={setButton}
+        city={city}
+        setCity={setCity}
+        handleBtn={handleBtn}
+        handleSubmit={handleSubmit}
+      />
       <NextFiveDays />
     </div>
   );
--- src/js/Popup.js
+++ src/js/Popup.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
 function Popup(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <section className={props.button ? "active_popup" : "popup"}>
+      <div className="cross" onClick={props.handleBtn}></div>
+      <form class="select_city_form">
+        <input
+          id="city_input"
+          type="text"
+          placeholder="Please enter a city..."
+          onFocus={(e) => (e.target.placeholder = "")}
+          onBlur={(e) => (e.target.placeholder = "Please enter a city...")}
+          onChange={(e) => {
+            props.setCity(e.target.value);
+          }}
+        />
+        <button id="submit_btn" type="submit" onClick={props.handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </section>
+  );
 }
 
 export default Popup;
